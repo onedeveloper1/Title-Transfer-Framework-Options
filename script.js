608b220a7f94ddeab76ec0fdb466ea8f3823809f
onedeveloper1 <whearn@oakland.edu>
2025-08-05 11:35:29 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('btn1').addEventListener('click', () => {
+    window.location.href = 'form.html';
+});
+
+document.getElementById('btn2').addEventListener('click', () => {
+    window.location.href = 'bot.html';
+});
+
+document.getElementById('btn3').addEventListener('click', () => {
+    window.location.href = 'web.html';
+});
+
